d01cc9d5003a070b01ef5c3cd3bbfd4b631be8b4
Hrvoje Tominovic <hrvoje.tominovic@ericsson.com>
2017-07-03 09:08:21 +0100

    Updates
    some minor updates

--- src/test/java/org/song/example/MockTest.java
+++ src/test/java/org/song/example/MockTest.java
@@ -44,32 +44,5 @@ public class MockTest {
 //	if the test cases do not have final or static methods to mock. TestNG will use different object factory
 //	to create the test case instances in the two cases. If we extend the "PowerMockTestCase" class when
 //	there is no final nor static methods to work with, the unit tests will not run consistently under Surefire in Maven.
-
-//	@PrepareForTest({AFinalClass.class, AStaticClass.class})
-//	public class MockTest extends PowerMockTestCase {
-//	    
-//	    @Test
-//	    public void mockFinalClassTest() {
-//	        AFinalClass tested = PowerMockito.mock(AFinalClass.class);
-//	        
-//	        final String testInput = "A test input";
-//	        final String mockedResult = "Mocked final echo result - " + testInput;
-//	        Mockito.when(tested.echoString(testInput)).thenReturn(mockedResult);
-//	        
-//	        // Assert the mocked result is returned from method call
-//	        Assert.assertEquals(tested.echoString(testInput), mockedResult);
-//	    }
-//	    
-//	    @Test
-//	    public void mockStaticClassTest() {
-//	        PowerMockito.mockStatic(AStaticClass.class);
-//	        
-//	        final String testInput = "A test input";
-//	        final String mockedResult = "Mocked static echo result - " + testInput;
-//	        Mockito.when(AStaticClass.echoString(testInput)).thenReturn(mockedResult);
-//	        
-//	        // Assert the mocked result is returned from method call
-//	        Assert.assertEquals(AStaticClass.echoString(testInput), mockedResult);
-//	    }
-
+//  see powermockito-testing MockTest.java
 }
